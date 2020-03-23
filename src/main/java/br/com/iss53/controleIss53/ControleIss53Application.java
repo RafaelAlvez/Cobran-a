a47b6390ad5c6e31e8734d46d91ab7706d7feb0a
unknown <38792522+RafaelAlvez@users.noreply.github.com>
2020-03-23 19:05:32 -0300

    First commit

---
+++ src/main/java/br/com/iss53/controleIss53/ControleIss53Application.java
@@ -0,0 +1,13 @@
+package br.com.iss53.controleIss53;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleIss53Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleIss53Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iss53/controleIss53/ControleIss53ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iss53.controleIss53;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleIss53ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
